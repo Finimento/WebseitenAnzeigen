b1b8dc3dc3ad6e5f2fd379e6f45cd3c8e461a9df
fiesematente <mosesfinimento@googlemail.com>
2024-10-24 09:43:27 +0200

    Initial Commit

---
+++ WebseitenAnzeigen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebseitenAnzeigen
+//
+//  Created by Mike Finimento on 24.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
